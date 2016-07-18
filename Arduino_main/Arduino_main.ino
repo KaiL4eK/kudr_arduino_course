8374abbcbfb2110945caae26dd516dfca142104f
KaiL4eK <darklexi494@gmail.com>
2016-07-19 01:55:19 +1000

    Encoder worked

--- Arduino_main/Arduino_main.ino
+++ Arduino_main/Arduino_main.ino
@@ -1,48 +1,75 @@
 #include <SoftwareSerial.h>
 SoftwareSerial BT(10, 11); // RX, TX
 
-int en   = 9,
-    in_p = 7,
-    in_n = 8;
+const byte  interruptPin = 2,
+            readIntPin   = 3,
+            en           = 9,
+            in_p         = 7,
+            in_n         = 8;
+
+volatile int turnage = 0;
+
+void hall_sensor() {
+  if (digitalRead(readIntPin) == HIGH) {
+    turnage++;
+  } else {
+    turnage--;
+  }
+}
 
 void setup()  
 {
   Serial.begin(9600);
   BT.begin(9600);
 
+  attachInterrupt(0, 
+                  hall_sensor, 
+                  RISING);
+
+  pinMode(interruptPin, INPUT_PULLUP);
+  pinMode(readIntPin, INPUT);
+  
   pinMode(en, OUTPUT);
   pinMode(in_p, OUTPUT);
   pinMode(in_n, OUTPUT);
-  
-  digitalWrite(en, HIGH);
+
+  analogWrite(en, 45);
   
   Serial.println("Ok, start =)");
 }
 
 void loop() 
 {
+  int prev_turn = 0;
   char a; // stores incoming character from other device
-  if (BT.available())
-  // if text arrived in from BT serial...
+  if (Serial.available())
+    // if text arrived in from BT serial...
   {
-    a=(BT.read());
-    Serial.print(a);
-    if (a=='w')
-    {
+    a=(Serial.read());
+    if (a=='w') {
+      turnage = 0;
       digitalWrite(in_p, HIGH);
       digitalWrite(in_n, LOW);
     }
-    if (a=='s')
-    {
+    if (a=='s') {
       digitalWrite(in_p, LOW);
       digitalWrite(in_n, HIGH);
     }
-    if (a==' ')
-    {
+    if (a==' ') {
       digitalWrite(in_p, LOW);
       digitalWrite(in_n, LOW);
     }
-    Serial.println(" <<<");   
-    // you can add more "if" statements with other characters to add more commands
+  }
+  // Count encoder on motor: 360 degrees == 75 ticks on rise/fall
+  // One rise or fall per rotation
+  if (turnage >= 75) {
+    digitalWrite(in_p, LOW);
+    digitalWrite(in_n, LOW);
+  }
+  
+  if ( prev_turn != turnage ) {
+    Serial.println(turnage);
+    prev_turn = turnage; 
   }
 }
+
