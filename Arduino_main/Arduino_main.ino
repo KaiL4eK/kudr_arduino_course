f5b6dbc654794b0782c46f91f4c453c9106d0010
KaiL4eK <darklexi494@gmail.com>
2016-07-18 21:21:54 +1000

    Motor intefrace

--- Arduino_main/Arduino_main.ino
+++ Arduino_main/Arduino_main.ino
@@ -1,40 +1,48 @@
 #include <SoftwareSerial.h>
-SoftwareSerial BT(10, 11); 
-// creates a "virtual" serial port/UART
-// connect BT module TX to D10
-// connect BT module RX to D11
-// connect BT Vcc to 5V, GND to GND
+SoftwareSerial BT(10, 11); // RX, TX
+
+int en   = 9,
+    in_p = 7,
+    in_n = 8;
+
 void setup()  
 {
-  // set digital pin to control as an output
-  pinMode(13, OUTPUT);
-  // set the data rate for the SoftwareSerial port
-  BT.begin(115200);
-  // Send test message to other device
-  BT.println("Hello from Arduino");
+  Serial.begin(9600);
+  BT.begin(9600);
+
+  pinMode(en, OUTPUT);
+  pinMode(in_p, OUTPUT);
+  pinMode(in_n, OUTPUT);
+  
+  digitalWrite(en, HIGH);
+  
+  Serial.println("Ok, start =)");
 }
-char a; // stores incoming character from other device
+
 void loop() 
 {
+  char a; // stores incoming character from other device
   if (BT.available())
   // if text arrived in from BT serial...
   {
     a=(BT.read());
-    if (a=='1')
+    Serial.print(a);
+    if (a=='w')
     {
-      digitalWrite(13, HIGH);
-      BT.println("LED on");
+      digitalWrite(in_p, HIGH);
+      digitalWrite(in_n, LOW);
     }
-    if (a=='2')
+    if (a=='s')
     {
-      digitalWrite(13, LOW);
-      BT.println("LED off");
+      digitalWrite(in_p, LOW);
+      digitalWrite(in_n, HIGH);
     }
-    if (a=='?')
+    if (a==' ')
     {
-      BT.println("Send '1' to turn LED on");
-      BT.println("Send '2' to turn LED on");
-    }   
+      digitalWrite(in_p, LOW);
+      digitalWrite(in_n, LOW);
+    }
+    Serial.println(" <<<");   
     // you can add more "if" statements with other characters to add more commands
   }
 }
